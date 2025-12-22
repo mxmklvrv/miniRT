74473602bd69a4cde0f4baa63a1f34e52948a009
Maxim Kleverov <mklevero@student.hive.fi>
2025-12-22 18:37:39 +0200

    update

--- inc/minirt.h
+++ inc/minirt.h
@@ -1,17 +1,20 @@
 #ifndef MINIRT_H
 # define MINIRT_H
 
-# include "macros.h"
-# include "structs.h"
 # include "libft.h"
+# include "macros.h"
 # include "mlx.h"
 # include "mlx_int.h"
-
-# include <stdio.h>
-# include <math.h>
-# include <sys/stat.h>
+# include "structs.h"
+# include <camera.h>
 # include <fcntl.h>
+# include <math.h>
+# include <scene.h>
+# include <sphere.h>
 # include <stdbool.h>
+# include <stdio.h>
+# include <sys/stat.h>
+# include <vector.h>
 
 int		handle_error(void);
 bool	check_args(int argc, char **argv, char **format);
