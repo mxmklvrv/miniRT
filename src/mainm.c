3b81a7fe64efa90aedaf96c1ff5070c94dd22de3
Maxim Kleverov <mklevero@student.hive.fi>
2026-01-09 16:37:06 +0200

    need to fix 0.

--- src/mainm.c
+++ src/mainm.c
@@ -442,7 +442,8 @@ int	is_valid_int(char *line)
 // checks the validity of the float nubmer
 // '-' accepted, also '+';
 // ony digits before and after '.'
-//0. pases but .0 is not ???? 
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!0. fixe 
+
 int	is_valid_float(char *line)
 {
 	int	i;
