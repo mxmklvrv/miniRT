cbf110ee0f47c6afb19f69292cb17bf6758a9b98
Maxim Kleverov <maximkleverov@icloud.com>
2026-02-16 14:34:32 +0200

    work in progress, in PW should work, not tested

--- src/movement.c
+++ src/movement.c
@@ -4,20 +4,19 @@
 #define ROTATE_SPEED 0.1f
 #define RESIZE_SPEED 0.2f
 
-#define KEY_W 119
-#define KEY_A 97
-#define KEY_S 115
-#define KEY_D 100
-#define KEY_Q 113
-#define KEY_E 101
-#define KEY_C 99
-#define KEY_SPACE 32
-#define KEY_TAB 65289
-#define KEY_LEFT 65361
-#define KEY_RIGHT 65363
-#define KEY_PLUS 61
-#define KEY_MINUS 45
-#define KEY_ESC 65307
+#define KEY_W 119 // forward
+#define KEY_A 97 // left
+#define KEY_S 115 // back
+#define KEY_D 100 // right
+#define KEY_Q 113 // up
+#define KEY_E 101 // down
+#define KEY_C 99 // cam 
+#define KEY_TAB 65289 // switch obj
+#define KEY_LEFT 65361 // rotate left
+#define KEY_RIGHT 65363 // rotate right
+#define KEY_PLUS 61 // resize up
+#define KEY_MINUS 45 // resize down
+#define KEY_ESC 65307 // esc
 
 typedef struct s_move_state
 {
@@ -40,7 +39,11 @@ int	key_press_hook(int key, t_data *data)
 	move = data->move_state;
 	if (key == KEY_TAB)
 		select_object(data->scene);
-	// for camcontrol here needed
+    else if(key == KEY_C)
+    {
+        data->control_cam = 1;
+        printf("controling cam");
+    }
 	else if (key == KEY_W)
 		move->forward = 1;
 	else if (key == KEY_S)
@@ -72,10 +75,7 @@ int	key_release_hook(int key, t_data *data)
 	t_move_state	*move;
 
 	move = data->move_state;
-	if (key == KEY_TAB)
-		select_object(data->scene);
-	// for camcontrol here needed
-	else if (key == KEY_W)
+	if (key == KEY_W)
 		move->forward = 0;
 	else if (key == KEY_S)
 		move->backward = 0;
@@ -96,6 +96,7 @@ int	key_release_hook(int key, t_data *data)
 		move->resize_up = 0;
 	else if (key == KEY_MINUS)
 		move->resize_down = 0;
+    apply_movement(data);
 	return (0);
 }
 
@@ -125,6 +126,22 @@ int	key_release_hook(int key, t_data *data)
 
  */
 
+
+
+
+void set_hooks(t_data *data)
+{
+    // check if malloc fails
+    // probably will do that in init 
+    data->move_state = ft_calloc(1, sizeof(t_move_state)); 
+    data->control_cam = 0;
+
+    mlx_hook(data->win, 2, 1L<<0, key_press_hook, data);
+    mlx_hook(data->win, 3, 1L<<1, key_release_hook, data);
+    mlx_hook(data->win, 17, 0, mlx_loop_end, data->mlx);
+
+}
+
 /**
  * If there is next obj - select it
  * Otherwise select the first one
@@ -137,6 +154,7 @@ void	select_object(t_scene *scene)
 		scene->obj_selected = scene->obj_selected->next;
 	else
 		scene->obj_selected = scene->obj_list;
+    printf("controlling obj"); // mb add coords of obj, type, etc
 }
 
 // need to create move bector with new_vec
@@ -165,24 +183,84 @@ void	translate_object(t_olist *node, t_vec3 move_vec)
 	}
 }
 
-void	handle_movement(int key, t_scene *scene)
+void    translate_cam(t_cam *cam, t_vec3 move_vec)
 {
-	t_vec3	move_vec;
+    cam->orient.origin = vector_add(cam->orient.origin, move_vec);
+    setup_camera_angle(cam);
+}
 
+
+void	apply_movement(t_data *data)
+{
+    t_move_state *move;
+	t_vec3	move_vec;
+    int need_redraw;
+
+    need_redraw = 0;
+    move = data->move_state;
 	move_vec = new_vector(0, 0, 0);
-	if (key == XK_w || key == XK_W) // key w
-		move_vec.z -= 1;
-	if (key == XK_s || key == XK_S) // key s
-		move_vec.z += 1;
-	if (key == XK_a || key == XK_A) // a
-		move_vec.x -= 1;
-	if (key == XK_d || key == XK_D) // d
-		move_vec.x += 1;
-	if (key == XK_q || key == XK_Q) // q
-		move_vec.y += 1;
-	if (key == XK_e || key == XK_E) // e
-		move_vec.y -= 1;
-	translate_object(scene->obj_selected, move_vec);
+
+    if(move->forward)
+        move_vec = vector_add(move_vec, new_vector(0,0,-MOVE_SPEED));
+    if(move->backward)
+        move_vec = vector_add(move_vec, new_vector(0,0,MOVE_SPEED));
+    if(move->left)
+        move_vec = vector_add(move_vec, new_vector(-MOVE_SPEED,0,0));
+    if(move->right)
+        move_vec = vector_add(move_vec, new_vector(MOVE_SPEED,0,0));
+    if(move->up)
+        move_vec = vector_add(move_vec, new_vector(0, MOVE_SPEED, 0));
+    if(move->down)
+        move_vec = vector_add(move_vec, new_vector(0, -MOVE_SPEED, 0));
+
+    // any checks after vector addition ?? 
+
+    if(data->control_cam)
+    {
+        translate_cam(&data->scene->cam, move_vec);
+        need_redraw = 1;
+    }
+    else
+    {
+        translate_object(data->scene->obj_selected, move_vec);
+        need_redraw = 1;
+    }
+
+    if(move->rotate_left)
+    {
+        if(data->scene->obj_selected && !data->control_cam)
+        {
+            rotate_objects(data->scene->obj_selected, -ROTATE_SPEED);
+            need_redraw = 1;
+        }
+    }
+    if(move->rotate_right)
+    {
+        if(data->scene->obj_selected && !data->control_cam)
+        {
+            rotate_objects(data->scene->obj_selected, ROTATE_SPEED);
+            need_redraw = 1;
+        }
+    }
+    if(move->resize_up)
+    {
+        if(data->scene->obj_selected && !data->control_cam)
+        {
+            resize_objects(data->scene->obj_selected, RESIZE_SPEED);
+            need_redraw = 1;
+        }
+    }
+    if(move->resize_down)
+    {
+        if(data->scene->obj_selected && !data->control_cam)
+        {
+            resize_objects(data->scene->obj_selected, -RESIZE_SPEED);
+            need_redraw = 1;
+        }
+    }
+	
+    if(need_redraw)
+        redraw_scene(data, data->scene);
 }
 
 // not sure about resizing
@@ -222,12 +300,12 @@ void	rotate_objects(t_olist *node, float angle)
 	if (node->obj_type == PL)
 	{
 		pl = (t_pl *)node->obj;
-		pl->normal.direction = rotate_y(pl->normal.direction, angle);
+		pl->normal.direction = vec_normalize(rotate_y(pl->normal.direction, angle));
 	}
 	else if (node->obj_type == CY)
 	{
 		cy = (t_cy *)node->obj;
-		cy->normal.direction = rotate_y(cy->normal.direction, angle);
+		cy->normal.direction = vec_normalize(rotate_y(cy->normal.direction, angle));
 	}
 }
 t_vec3	rotate_y(t_vec3 current, float angle)
@@ -247,4 +325,3 @@ t_vec3	rotate_y(t_vec3 current, float angle)
 
 
 
-	// need camera movement
