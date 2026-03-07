7abea4a11434505163a9933a40810bda1d79b868
Anna Kolupaeva <akolupae@c1r2p6.hive.fi>
2026-03-07 20:57:30 +0200

    Added arctan to calculations, now nothing works

--- src/draw.c
+++ src/draw.c
@@ -53,16 +53,21 @@ static void	draw_one_render_cycle(t_data *data, int pixels_to_render, bool *firs
 static t_vec3	get_direction_for_position(t_pixel pixel, t_cam cam)
 {
 	t_vec3		direction;
+	t_matrix	rotation_i;
+	t_matrix	rotation_j;
 	t_matrix	rotation;
-	float		multiplier;
+	float		angle;
 
-
-	multiplier = degrees_to_radians(cam.fov) / ft_max(2, WIDTH, HEIGHT);
-	rotation = new_rotation_y_matrix((pixel.i - WIDTH / 2) * multiplier);
+	angle = -atanf((pixel.i - WIDTH / 2) * cam.pixel_size);
+	if (pixel.i == 0 && pixel.j == 0)
+		printf("Angle 0x: %f\n", angle);
+	rotation_i = new_rotation_z_matrix(angle);
+	angle = atanf((pixel.j - HEIGHT / 2) * cam.pixel_size);
+	if (pixel.i == 0 && pixel.j == 0)
+		printf("Angle 0z: %f\n", angle);
+	rotation_j = new_rotation_x_matrix(angle);
+	rotation = chain_matrices(rotation_i, rotation_j);
 	direction = matrix_multiply_by_vector(rotation, cam.orient.direction);
-	free_matrix(rotation);
-	rotation = new_rotation_x_matrix((pixel.j - HEIGHT / 2) * multiplier);
-	direction = matrix_multiply_by_vector(rotation, direction);
 	free_matrix(rotation);
 	direction = vector_normalize(direction);
 	return (direction);
