a79eb15fc671ba73b844212c1685554ab35f430f
Maxim Kleverov <maximkleverov@icloud.com>
2026-03-20 13:53:52 +0200

    update

--- src/parsing/parse_camera.c
+++ src/parsing/parse_camera.c
@@ -36,30 +36,30 @@
 // 	return (0);
 // }
 
-int	parse_cam(char *line, t_scene *scene)
+int parse_cam(char *line, t_scene *scene)
 {
-	char	**res;
-	float	fov;
+    char **res;
+    float fov;
 
-	if (scene->qt_cam > 1)
-		return (error_return(ERR_CAM_QTY, scene->err_m));
-	if (count_elements(line) != 3)
-		return (error_return(ERR_CAM_SPEC, scene->err_m));
-	res = ft_split(line, ' ');
-	if (!res)
-		return (error_return(ERR_ALLOC, NULL));
-	if (parse_vector(res[0], &scene->cam.orient.origin, -100.0f, 100.0f) == 1)
-		return (parse_error(scene, ERR_CAM_VIEW, res, NULL));
-	scene->cam.orient.origin.w = 1;
-	if (parse_vector(res[1], &scene->cam.orient.direction, -1.0f, 1.0f) == 1)
-		return (parse_error(scene, ERR_CAM_ORIT, res, NULL));
-	scene->cam.orient.direction.w = 0;
-	fov = 0;
-	if (parse_float(res[2], 0.0f, 180.0f, &fov) == 1)
-		return (parse_error(scene, ERR_CAM_FOV, res, NULL));
-	scene->cam.fov = fov;
-    scene->cam.yaw= atan2f(cam->orient.direction.x, cam->orient.direction.z); // added for test
-	scene->cam.pitch = asinf(cam->orient.direction.y); // adde for test
-	free_array(res);
-	return (0);
+    if (scene->qt_cam > 1)
+        return (error_return(ERR_CAM_QTY, scene->err_m));
+    if (count_elements(line) != 3)
+        return (error_return(ERR_CAM_SPEC, scene->err_m));
+    res = ft_split(line, ' ');
+    if (!res)
+        return (error_return(ERR_ALLOC, NULL));
+    if (parse_vector(res[0], &scene->cam.orient.origin, -100.0f, 100.0f) == 1)
+        return (parse_error(scene, ERR_CAM_VIEW, res, NULL));
+    scene->cam.orient.origin.w = 1;
+    if (parse_vector(res[1], &scene->cam.orient.direction, -1.0f, 1.0f) == 1)
+        return (parse_error(scene, ERR_CAM_ORIT, res, NULL));
+    scene->cam.orient.direction.w = 0;
+    fov = 0;
+    if (parse_float(res[2], 0.0f, 180.0f, &fov) == 1)
+        return (parse_error(scene, ERR_CAM_FOV, res, NULL));
+    scene->cam.fov = fov;
+    scene->cam.yaw = atan2f(scene->cam.orient.direction.x, scene->cam.orient.direction.z); // added for test
+    scene->cam.pitch = asinf(scene->cam.orient.direction.y);                               // adde for test
+    free_array(res);
+    return (0);
 }
