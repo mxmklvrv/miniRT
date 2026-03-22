a136749dbe9772055ffb4e870f29feb2b2adc680
Anna Kolupaeva <akolupae@c1r6p19.hive.fi>
2026-03-22 16:30:04 +0200

    Fixed specular liighting

--- src/tracing_lighting.c
+++ src/tracing_lighting.c
@@ -4,7 +4,7 @@
  * ambient = 0.1;
  * diffuse = 0.9;
  * specular = 0.9;
- * shininess = 200.0.
+ * shininess = 32.0.
  */
 int	lighting(t_light light, t_ambient background, t_intersection intersection, t_ray ray)
 {
@@ -20,7 +20,7 @@ int	lighting(t_light light, t_ambient background, t_intersection intersection, t
 	float	reflect_angle;
 	float	shininess;
 
-	color = color_mix(intersection.shape->color, light.color, light.bright);
+	color = intersection.shape->color;//color_mix(intersection.shape->color, light.color, light.bright);
 	point = get_ray_point(ray, get_closest_hit(intersection));
 	light_vector = vector_normalize(vector_substract(light.pos, point));
 	ambient = color_mix(color, background.color, background.amb);
@@ -34,8 +34,9 @@ int	lighting(t_light light, t_ambient background, t_intersection intersection, t
 	else
 	{
 		diffuse = color_mix(color, light.color, light_angle * light.bright);
+		diffuse = color_mix(background.color, diffuse, light_angle);
 		reflect_vector = vector_reflect(vector_negate(light_vector), normal_vector);
-		reflect_angle = vector_dot(reflect_vector, ray.direction);
+		reflect_angle = vector_dot(reflect_vector, vector_negate(ray.direction));
 		if (reflect_angle < 0 || is_equalf(reflect_angle, 0))
 			specular = 0;
 		else
