1f86b3c7dfde5fb7859921c7586b7bbd8310a916
Maxim Kleverov <maximkleverov@icloud.com>
2026-03-25 12:39:56 +0200

    added radius, half h

--- src/parsing/parse_cy.c
+++ src/parsing/parse_cy.c
@@ -88,12 +88,14 @@ int	init_cylinder(t_shape *shape, char **res, t_scene *scene)
 	if (parse_vector(res[1], &shape->normal.direction, -1.0f, 1.0f) == 1)
 		return (error_return(ERR_CY_AXIS, scene->err_m));
     shape->normal.direction.w = 0;
-	// should i normilize it here ?
 	if (parse_float(res[2], 0.0f, 100.0f, &shape->diameter) == 1)
 		return (error_return(ERR_CY_DIAM, scene->err_m));
 	if (parse_float(res[3], 0.0f, 100.0f, &shape->height) == 1)
 		return (error_return(ERR_CY_HGHT, scene->err_m));
 	if (parse_rgb(res[4], &shape->color) == 1)
 		return (error_return(ERR_CY_COLR, scene->err_m));
+    shape->axis = vector_normalize(shape->normal.direction);
+	shape->radius = shape->diameter / 2.0f;
+	shape->half_h = shape->height / 2.0f;
 	return (0);
 }
