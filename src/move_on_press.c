be0f242621b28773f3f4f00f579b534bf7353264
Maxim Kleverov <maximkleverov@icloud.com>
2026-03-31 14:44:08 +0300

    for multiple lights

--- src/move_on_press.c
+++ src/move_on_press.c
@@ -50,6 +50,29 @@ void	set_general_keys(int key, t_data *data)
 	if (key == KEY_ESC)
 		mlx_loop_end(data->mlx);
 }
+/*
+// new for multiple lights 
+void	set_general_keys(int key, t_data *data)
+{
+	if (key == KEY_TAB)
+	{
+        if (data->control_light)
+            select_light(data->scene);
+		else if (data->control_cam)
+			switch_to_obj(data);
+		else
+			select_object(data);
+	}
+	else if (key == KEY_C)
+		toggle_cam(data);
+	else if (key == KEY_L)
+		toggle_light(data);
+	if (key == KEY_ESC)
+		mlx_loop_end(data->mlx);
+}
+// new for multiple lights up
+*/
+
 
 static void	switch_to_obj(t_data *data)
 {
@@ -161,6 +184,52 @@ void	translate_light(t_light *light, t_vec3 move_vec)
 		return ;
 	light->pos = vector_add(light->pos, move_vec);
 }
+/*
+// new functions for multiple lights down 
+void    select_light(t_scene *scene)
+{
+    if(!scene->light_selected)
+        scene->light_selected = scene->light_list;
+    else if (scene->light_selected->next)
+        scene->light_selected = scene->light_selected->next;
+    else
+        scene->light_selected = scene->light_list;
+}
+
+void translate_light(t_llist *node, t_vec3 move_vec) // new for multiple lights
+{
+    if(!node || !node->light)
+        return ;
+    node->light->pos = vector_add(node->light->pos, move_vec);
+}
+
+int	handle_translation(t_data *data)
+{
+	t_move_state	*move;
+	t_vec3			move_vec;
+
+	move = data->move_state;
+	move_vec = new_vector(0, 0, 0);
+	if (data->control_cam)
+		cam_move_calculation(data, &move_vec, move);
+	else if (data->control_light)
+		obj_move_calculation(&move_vec, move);
+	else
+		obj_move_calculation(&move_vec, move);
+	if (move_vec.x != 0 || move_vec.y != 0 || move_vec.z != 0)
+	{
+		if (data->control_cam)
+			translate_cam(&data->scene->cam, move_vec);
+		else if (data->control_light && data->scene->light_selected)
+            translate_light(data->scene->light_selected, move_vec);
+		else if (data->scene->obj_selected)
+			translate_object(data->scene->obj_selected, move_vec);
+		return (1);
+	}
+	return (0);
+}
+*/
+// new functions up
 
 void	apply_movement(t_data *data)
 {
