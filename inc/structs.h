f04a8970abc4b515366968ed50486ff7f93d6fbe
Maxim Kleverov <maximkleverov@icloud.com>
2026-03-31 14:45:38 +0300

    new struct for lights

--- inc/structs.h
+++ inc/structs.h
@@ -71,6 +71,12 @@ typedef struct s_olist
 	struct s_olist	*next;
 }	t_olist;
 
+typedef struct s_llist // added new 
+{
+    t_light *light;
+    struct  s_llist *next;
+} t_llist;
+
 typedef struct s_intersection
 {
 	int		count;
@@ -109,6 +115,7 @@ typedef struct s_light
 typedef struct s_scene
 {
 	t_olist			*obj_list;
+    t_llist         *light_list; // added new 
 	t_ambient		ambient;
 	t_light			light;
 	t_cam			cam;
@@ -118,6 +125,7 @@ typedef struct s_scene
     int             next_obj_id;
 	char			*error_line;
 	t_olist			*obj_selected;
+    t_llist         *light_selected; // added new 
 }					t_scene;
 
 typedef struct s_data
